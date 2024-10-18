f1b3dda4a62ab6a729321b0bb0e99d2fedfae51c
Shakil Alam <itxshakil@gmail.com>
2024-10-18 09:54:04 +0530

    fix -Remove errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,39 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AppContext } from './context/AppContext'
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
 
-  const { adminToken } = useContext(AppContext)
+  const { adminToken } = useContext(AdminContext)
 
-  return (
-    <div>
+  return adminToken ? (
+    <div className='bg-[#f8f9fd]'>
+      <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
       <Login />
       <ToastContainer />
-    </div>
+    </>
   )
 }
 
--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,17 +1,54 @@
 import { createContext, useState } from "react";
-
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-      const [adminToken, setAdminToken] = useState('')
-
+      const [adminToken, setAdminToken] = useState(localStorage.getItem('adminToken') ? localStorage.getItem('adminToken') : '')
+      const [doctors, setDoctors ] = useState([])
       const backendUrl = import.meta.env.VITE_BACKEND_URL
 
+      const getAllDoctors = async () => {
+            try {
+                  const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers:{adminToken}})
+            
+                  if(data.success) {
+                        setDoctors(data.doctors)
+                        console.log(data.doctors)
+                  }
+                  else {
+                        toast.error(data.message)
+                  }
+            } catch (error) {
+                 toast.error(error.message) 
+            }
+      }
+
+      const changeAvailability = async (docId) => {
+            try {
+                  const { data } = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers:{adminToken}})
+            
+                  if(data.success) {
+                        toast.success(data.message)
+                        getAllDoctors()
+                  }
+                  else {
+                        toast.error(data.message)
+                  } 
+            } catch (error) {
+                  toast.error(error.message) 
+            }
+      }
+
       const value = {
-            adminToken, setAdminToken,
-            backendUrl
+            adminToken, 
+            setAdminToken,
+            backendUrl,
+            doctors,
+            getAllDoctors,
+            changeAvailability,
 
       }
 
