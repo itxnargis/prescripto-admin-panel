4998d6c150196c374e5cf0c9a8a28c0219b835cd
itxnargis <itxnargiskhatun@gmail.com>
2024-11-06 15:56:48 +0530

    fix - App context error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/AdminContext.jsx'
 import DoctorContextProvider from './context/DoctorContext.jsx'
-import AppContextProvider from './context/appContext.jsx'
+import AppContextProvider from '<div className="" />
+<context />
+<A></A>ppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
